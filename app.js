96c1f157cfa1cfcf9d2736666622ae7cbf2d9f3a
jr-RobertoGomes <robertoti.adm@gmail.com>
2022-11-14 11:22:15 -0300

    Atualizado app.js - Nova Funcao
    > function telaLogin

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Ola mundo");
+const main = document.querySelector("#main");
+
+function telaLogin(){
+
+    var form = document.createElement("form");
+    form.textContent = 'Forumlario de login';
+
+    var idInputs = ["form-inp-usuario","form-inp-senha","form-btn-login"]
+
+    for (var i = 0 ; i < 3 ; i++){
+        var input = document.createElement("input");
+        var id = idInputs[i];
+
+        if (id !== "form-btn-login"){
+            
+            input.setAttribute("id",id);
+            input.setAttribute("type","text");
+
+        
+        } else {
+
+            input.setAttribute("id",id);
+            input.setAttribute("type","submit");
+        }
+
+        form.insertBefore(input,null);
+    }
+
+    main.insertBefore(form,null);
+
+}

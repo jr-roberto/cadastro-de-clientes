1a3c792d0a475eda8db3b372ab8c4af28b31095e
jr-roberto <robertoti.adm@gmail.com>
2022-11-26 13:54:52 -0300

    Atualizacoes necessarias

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 const main = document.querySelector("#main");
 
-setTimeout(login,1000);
+setTimeout(login,100);
--- views.js
+++ views.js
@@ -2,8 +2,12 @@ function login() {
     var form = document.createElement("div");
     var styleForm = form.style;
     form.setAttribute("id","form-login");
-    styleForm.width = "450px";
-    styleForm.height = "400px";
+    styleForm.position = "absolute";
+    styleForm.width = "50vw";
+    styleForm.height = "60vh";
+    styleForm.left = "50%";styleForm.top = "50%";
+    styleForm.transform = "translate(-50%,-50%)"
+    styleForm.background = "#000";
 
     main.insertBefore(form,null);
 }

fba65511f5fe25edd02024f899bc9f967b7f07e3
Roberto Gomes <91014834+jr-roberto@users.noreply.github.com>
2022-12-13 15:16:36 -0300

    Ajustes

--- views.js
+++ views.js
@@ -2,6 +2,10 @@ function login() {
     var form = document.createElement("div");
     var styleForm = form.style;
     form.setAttribute("id","form-login");
+    form.innerHTML = "Click - me";
+    styleForm.fontSize = "50px";
+    styleForm.textAlign = "center";
+    styleForm.top = "20px";
     styleForm.position = "absolute";
     styleForm.width = "50vw";
     styleForm.height = "60vh";

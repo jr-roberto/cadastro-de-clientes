55527a8870901ab895e88b314f71601d59ffc65a
Roberto Gomes <91014834+jr-roberto@users.noreply.github.com>
2022-12-15 19:52:51 -0300

    Alteracoes necessaria

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var num = [0,1,2,3,4,5,6,7,8,9]
 var sorteio = ()=>{
 
     main.innerHTML = Math.floor(Math.random() * num.length);
-    
+
     setTimeout(sorteio,5000)
 }
+
+setTimeout(sorteio, 100);

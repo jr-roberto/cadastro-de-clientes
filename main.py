fc60589ee48fe57aff06638ef759b05b9014cee6
jr-roberto <robertoti.adm@gmail.com>
2022-12-30 22:33:27 -0300

    Atualizacao necessaria

--- main.py
+++ main.py
@@ -9,4 +9,18 @@ if 'db' not in os.listdir():
 
 eel.init('web')
 
-eel.start('index.hrml')
+@eel.expose
+def table_user():
+    table = db.carregar_tabela()
+    return table
+
+@eel.expose
+def novo_cadastro(nome_usuario,senha,st='Ativo'):
+    
+    db.novo_usuario(nome_usuario,senha,st)
+
+    eel.load_table()()
+    
+    return db.carregar_tabela()
+
+eel.start('index.html')
